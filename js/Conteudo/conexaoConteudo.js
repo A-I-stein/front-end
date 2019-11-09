1cbbd13381bd40edbd21c4172a1b71e75f103fd3
Italo <ifvnasc@gmail.com>
2019-11-09 19:19:38 -0300

    Alguem testa isso aqui

--- js/Conteudo/conexaoConteudo.js
+++ js/Conteudo/conexaoConteudo.js
@@ -1,83 +1,75 @@
 function buscarTodosConteudos(){
   let conexao = Request.get("req=all", "ConteudoServlet");
-  window.setTimeout(
-    function() {
       conexao.then(function(resultado){
         let conteudoVeto = [];
         for (var i = 0; i < resultado.length; i++) {
           conteudoVeto[i] = transformaEmConteudo(JSON.parse(resultado[i]));
         }
-      imprimeTodosConteudos(resultado);
-    }
-    )}, Math.random() * 2000 + 1000);
+      imprimeTodosConteudos(conteudoVeto);
+    });
+
 }
 
 function buscarConteudoMateria(materia) {
   let conteudoJSON = encapsularBuscarConteudoMateria(materia);
   let conexao = Request.get("tipo="+conteudoJSON+"&req=materia", "ConteudoServlet");
-  window.setTimeout(
-    function() {
+
       conexao.then(function(resultado) {
+        let conteudoVeto = [];
         for (var i = 0; i < resultado.length; i++) {
-          resultado[i] = transformaEmConteudo(resultado[i]);
+          conteudoVeto[i] = transformaEmConteudo(JSON.parse(resultado[i]));
         }
-        imprimeTodosConteudo(resultado);
-      }
-    )}, Math.random() * 2000 + 1000);
+        imprimeTodosConteudo(conteudoVeto);
+    });
 }
 
 function buscarConteudoTipo(tipo) {
   let conteudoJSON = encapsularBuscarConteudoTipo(tipo);
   let conexao = Request.get("tipo="+conteudoJSON+"&req=materia", "ConteudoServlet");
-  window.setTimeout(
-    function() {
+
       conexao.then(function(resultado) {
         for (var i = 0; i < resultado.length; i++) {
-          resultado[i] = transformaEmConteudo(resultado[i]);
+        let conteudoVeto = [];
+          conteudoVeto[i] = transformaEmConteudo(JSON.parse(resultado[i]));
         }
-        imprimeTodosConteudo(resultado);
-      }
-    )}, Math.random() * 2000 + 1000);
+        imprimeTodosConteudo(conteudoVeto);
+    });
 }
 function buscarConteudoNome(topico){
   let conteudoJSON = encapsularBuscarConteudoNome(topico);
   let conexao = Request.get("tipo="+conteudoJSON+
               "&req=especifco", "ConteudoServlet");
-  window.setTimeout(
-    function() {
+
       conexao.then(function(resultado) {
-        resultado = transformaEmConteudo(resultado);
-        imprimeTodosConteudo(resultado);
-      }
-    )}, Math.random() * 2000 + 1000);
+        let conteudoVeto;
+        conteudoVeto = transformaEmConteudo(JSON.parse(resultado));
+        imprimeTodosConteudo(conteudoVeto);
+  });
 }
 
 function buscarConteudoUsername(username){
   let conteudoJSON = encapsularBuscarConteudoUsername(username);
   let conexao = Request.get("tipo="+conteudoJSON+
               "&req=usuario", "ConteudoServlet");
-  window.setTimeout(
-    function() {
+
       conexao.then(function(resultado) {
-        for (var i = 0; i < rasultado.length; i++) {
-          rasultado[i] = transformaEmConteudo(resultado[i]);
+        let conteudoVeto = [];
+        for (var i = 0; i < resultado.length; i++) {
+          resultado[i] = transformaEmConteudo(JSON.parse(resultado[i]));
         }
-        imprimeTodosConteudo(resultado);
-      }
-    )}, Math.random() * 2000 + 1000);
+        imprimeTodosConteudo(conteudoVeto);
+    });
 }
 
 function buscarConteudoCodigo(codigo){
   let conteudoJSON = encapsularBuscarConteudoCodigo(codigo);
   let conexao = Request.get("tipo="+conteudoJSON+
               "&req=codigo", "ConteudoServlet");
-  window.setTimeout(
-    function() {
       conexao.then(function(resultado) {
-            resultado = transformaEmConteudo(resultado);
-            imprimeConteudo(resultado);
-      }
-    )}, Math.random() * 2000 + 1000);
+        let conteudoVeto;
+            conteudoVeto = transformaEmConteudo(JSON.parse(resultado));
+            imprimeConteudo(conteudoVeto);
+    });
 }
 
 function transformaEmConteudo(resultado){
