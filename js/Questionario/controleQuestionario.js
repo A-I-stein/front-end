a5a83ae151bdd2c84613c25fc240e2d50b35f07b
Gabriel <gabriel.gomesgalo@gmail.com>
2019-11-13 19:31:47 -0300

    Questionario V.07
    Melhoro

--- js/Questionario/controleQuestionario.js
+++ js/Questionario/controleQuestionario.js
@@ -6,13 +6,25 @@ function inserirQuestionario(questionario){
   console.log(questionario);
   vetorQuestoes[contador] = questionario;
   contador = contador + 1;
-  if (contador == 9) {
+  if (contador == 10) {
     contador = 0;
     imprimirQuestionario();
   }
 }
 
 function imprimirQuestionario(){
+  if (contador == 0) {
+    botaoRetornarQuestao[0].style.display = "none";
+  }
+  else{
+    botaoRetornarQuestao[0].style.display = "block";
+  }
+  if (contador == 9) {
+    botaoProximaQuestao[0].style.display = "none";
+  }
+  else{
+    botaoProximaQuestao[0].style.display = "block";
+  }
   secaoEscolha.style.display = "none";
   secaoQuestoes.style.display = "block";
 
@@ -38,48 +50,22 @@ function imprimirQuestionario(){
   }
 
   inputPergunta[0].innerHTML = vetorQuestoes[contador].descPergunta;
-
-
-
-  let botaoProximaQuestao = document.getElementsByName("proximaQuestao"),
-      botaoRetornarQuestao = document.getElementsByName("retornarQuestao");
-
-
-  botaoProximaQuestao[0].addEventListener("click", function () {
-
-
-    for (var i = 0; i < respostaEscolhida.length; i++) {
-      if (respostaEscolhida[i].checked) {
-        escolha = respostaEscolhida[i].value;
-        respostaEscolhida[i].checked = false;
-      }
-    }
-    console.log(escolha);
-
-    if (escolha != null) {
-      vetorRespostasSalvas[contador] = escolha;
-      console.log(contador);
-      escolha = null;
-      contador ++;
-      imprimirQuestionario();
-    }
-
-  })
-
-  botaoRetornarQuestao[0].addEventListener("click", function () {
-
-      contador = contador - 1;
-      console.log(contador);
-      imprimirQuestionario();
-
-  })
-
-
-
-
+  console.log(contador);
 }
 
+function finalizarQuestionario() {
+  let acertos = 0;
 
+  for (var i = 0; i < vetorQuestoes.length; i++) {
+    console.log("corettas: " +vetorQuestoes[i].descRespCorreta);
+    console.log("escolhas: " +vetorRespostasSalvas[i]);
+    if (vetorQuestoes[i].descRespCorreta == vetorRespostasSalvas[i]) {
+        acertos++;
+    }
+  }
+  console.log(acertos);
+
+}
 
 function shuffle(a) {
     for (let i = a.length - 1; i > 0; i--) {
@@ -93,17 +79,67 @@ function shuffle(a) {
 
 let botaoIniciarQuestionario = document.getElementsByName("botaoIniciarQuestionario"),
     secaoQuestoes = document.querySelector(".questoes"),
-    secaoEscolha = document.querySelector(".modalEscolha");
+    secaoEscolha = document.querySelector(".modalEscolha"),
+    botaoProximaQuestao = document.getElementsByName("proximaQuestao"),
+    botaoRetornarQuestao = document.getElementsByName("retornarQuestao"),
+    botaoFinalizarQuestionario = document.getElementsByName("finalizar"),
+    respostaEscolhida = document.getElementsByName("Resposta"),
+        escolha = null;
 
 
 
 
   botaoIniciarQuestionario[0].addEventListener("click", function(){
     let materia = document.getElementsByName("materiaEscolha");
-        console.log(materia[0]);
+        console.log( materia[0].options[materia[0].selectedIndex].value);
     materia = materia[0].options[materia[0].selectedIndex].value;
 
       buscarQuestionarioMateria(materia, function(conteudo){
           inserirQuestionario(conteudo);
+      })
+
+      botaoProximaQuestao[0].addEventListener("click", function () {
+
+
+        for (var i = 0; i < respostaEscolhida.length; i++) {
+          if (respostaEscolhida[i].checked) {
+            escolha = respostaEscolhida[i].value;
+            respostaEscolhida[i].checked = false;
+          }
+        }
+        console.log(escolha);
+
+        if (escolha != null) {
+          vetorRespostasSalvas[contador] = escolha;
+          console.log(contador);
+          escolha = null;
+          contador ++;
+          console.log(contador);
+          imprimirQuestionario();
+        }
+
+      })
+
+      botaoRetornarQuestao[0].addEventListener("click", function () {
+        for (var i = 0; i < respostaEscolhida.length; i++) {
+            respostaEscolhida[i].checked = false;
+        }
+          console.log(contador);
+          contador = contador - 1;
+          console.log(contador);
+          imprimirQuestionario();
+
+      })
+
+      botaoFinalizarQuestionario[0].addEventListener("click", function(){
+        for (var i = 0; i < respostaEscolhida.length; i++) {
+          if (respostaEscolhida[i].checked) {
+            escolha = respostaEscolhida[i].value;
+            respostaEscolhida[i].checked = false;
+          }
+        }
+        vetorRespostasSalvas[contador] = escolha;
+        finalizarQuestionario();
+        console.log(vetorRespostasSalvas);
       })
   })
